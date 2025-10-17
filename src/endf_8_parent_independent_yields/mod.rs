cec51e21bd0a14c9f801f3fda7661eee4208e781
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2025-10-17 14:48:06 +0800

    added parent independent fission yield data for neptunium

--- src/endf_8_parent_independent_yields/mod.rs
+++ src/endf_8_parent_independent_yields/mod.rs
@@ -1,18 +1,27 @@
-/// u235 fission yields
-pub(crate) mod u235;
 
+// uranium fission yields
 /// u232 fission yields
 pub(crate) mod u232;
 /// u233 fission yields
 pub(crate) mod u233;
 /// u234 fission yields
 pub(crate) mod u234;
+/// u235 fission yields
+pub(crate) mod u235;
 /// u236 fission yields
 pub(crate) mod u236;
 /// u237 fission yields
 pub(crate) mod u237;
 /// u238 fission yields
 pub(crate) mod u238;
+
+
+// neptunium fission yields
+/// np237 fission yields
+pub(crate) mod np237;
+/// np238 fission yields
+pub(crate) mod np238;
+
 
 /// holds the enums for various nuclides
 pub mod nuclides;
