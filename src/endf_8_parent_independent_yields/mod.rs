45fbcede99c1c26ac8769b124149e40b6c66f0df
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2025-10-17 14:40:39 +0800

    added u232, u233, u234 csv tables from janis

--- src/endf_8_parent_independent_yields/mod.rs
+++ src/endf_8_parent_independent_yields/mod.rs
@@ -1,6 +1,19 @@
 /// u235 fission yields
 pub(crate) mod u235;
 
+/// u232 fission yields
+pub(crate) mod u232;
+/// u233 fission yields
+pub(crate) mod u233;
+/// u234 fission yields
+pub(crate) mod u234;
+/// u236 fission yields
+pub(crate) mod u236;
+/// u237 fission yields
+pub(crate) mod u237;
+/// u238 fission yields
+pub(crate) mod u238;
+
 /// holds the enums for various nuclides
 pub mod nuclides;
 
