6dc40bf3e347f363068fc3ff28c67af1f1da7baf
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2025-10-17 14:10:52 +0800

    reformatted the prelude to make it neater

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,7 @@
 use uom::si::f64::*;
 
-use crate::endf_8_parent_independent_yields::{energy_interpolation::linear_linear_energy_interpolation, nuclides::Nuclide};
+use crate::endf_8_parent_independent_yields::nuclides::Nuclide;
+use crate::endf_8_parent_independent_yields::energy_interpolation::linear_linear_energy_interpolation;
 
 pub fn fission_yield(fissioning_nuclide: Nuclide,
     fission_product_nuclide: Nuclide,
