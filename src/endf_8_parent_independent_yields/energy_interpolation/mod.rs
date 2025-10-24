167c4414a6d08e758f9ac4b2f42a2b738d718fa5
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2025-10-24 15:30:44 +0800

    implemented np237 data, to be tested

--- src/endf_8_parent_independent_yields/energy_interpolation/mod.rs
+++ src/endf_8_parent_independent_yields/energy_interpolation/mod.rs
@@ -1,5 +1,6 @@
 use uom::si::f64::*;
 
+use crate::endf_8_parent_independent_yields::np237::linear_linear_energy_interpolation_np237;
 use crate::endf_8_parent_independent_yields::nuclides::Nuclide;
 use crate::endf_8_parent_independent_yields::pu239::linear_linear_energy_interpolation_pu239;
 use crate::endf_8_parent_independent_yields::th232::linear_linear_energy_interpolation_th232;
@@ -30,6 +31,9 @@ pub fn linear_linear_energy_interpolation(
         },
         Nuclide::Th232 => {
             return linear_linear_energy_interpolation_th232(neutron_energy, fission_product_nuclide);
+        },
+        Nuclide::Np237 => {
+            return linear_linear_energy_interpolation_np237(neutron_energy, fission_product_nuclide);
         },
         _ => todo!("fissioning nuclide interpolation not implemented"),
     };
