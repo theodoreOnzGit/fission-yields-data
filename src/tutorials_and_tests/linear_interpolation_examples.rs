cbb7aa7a591b419bdbc20fd547bb9d7a50cb73f5
teddy0@snrsiDesktop <snrokct@nus.edu.sg>
2025-10-24 12:07:39 +0800

    pu239 tested interpolation between 0.0253 eV and 500 keV

--- src/tutorials_and_tests/linear_interpolation_examples.rs
+++ src/tutorials_and_tests/linear_interpolation_examples.rs
@@ -56,10 +56,10 @@ fn linear_interpolation_example_2(){
 
     // first we decide our nuclides
     let cesium_137 = Nuclide::Cs137;
-    let uranium_233 = Nuclide::U235;
+    let uranium_235 = Nuclide::U235;
     let neutron_energy = Energy::new::<megaelectronvolt>(3.0);
     let test_yield: f64 = fission_yield(
-        uranium_233, 
+        uranium_235, 
         cesium_137, 
         neutron_energy).get::<ratio>();
 
@@ -87,6 +87,70 @@ fn linear_interpolation_example_2(){
     let reference_yield: f64 = 
         fast_yield + 
         (high_yield - fast_yield) * 
+        (numerator/denominator).get::<ratio>();
+
+    // if coded correctly, the test yield should equal the reference yield 
+    approx::assert_relative_eq!(
+        reference_yield,
+        test_yield,
+        max_relative=1e-12
+    );
+
+}
+
+/// now, for Pu239, 
+/// there are four data points provided for fission yields
+///
+/// 1. 0.0253 eV
+/// 2. 500.0 keV
+/// 3. 2.0 MeV
+/// 4. 14.0 MeV
+///
+/// we linearly interpolate between the yields at each of these values for 
+/// pu239
+///
+/// let's use Xe-135 as an example, which is an important reactor poison
+///
+/// first we interpolate at 1 keV
+#[test]
+fn linear_interpolation_example_3(){
+
+    // first we decide our nuclides
+    let xenon_135 = Nuclide::Xe135;
+    let plutonium_239 = Nuclide::Pu239;
+    let neutron_energy = Energy::new::<kiloelectronvolt>(1.0);
+    let test_yield: f64 = fission_yield(
+        plutonium_239, 
+        xenon_135, 
+        neutron_energy).get::<ratio>();
+
+    // now behind the scenes, we are linearly interpolating the yields 
+    // between 0.0253 eV and 500 keV
+
+    let thermal_energy = Energy::new::<electronvolt>(0.0253);
+    let fast_energy = Energy::new::<kiloelectronvolt>(500.0);
+    let _two_mev_energy = Energy::new::<megaelectronvolt>(2.0);
+    let _high_energy = Energy::new::<megaelectronvolt>(14.0);
+
+    // taken straight from endf 8 libraries, the thermal and fast yields are
+    // for cs-137:
+    //
+    // 
+    let thermal_yield: f64 = 0.00314131;
+    let fast_yield: f64 = 0.006140546;
+    let _two_mev_yield: f64 = 0.006147635;
+    // the high yield for 14 MeV neutron is also provided for reference here
+    let _high_yield: f64 = 0.006461199;
+
+
+    // using linear interpolation, we calculate the reference yield
+
+    let numerator: Energy = neutron_energy - thermal_energy; 
+    let denominator: Energy = fast_energy - thermal_energy;
+
+    let reference_yield: f64 = 
+        thermal_yield + 
+        (fast_yield - thermal_yield) * 
         (numerator/denominator).get::<ratio>();
 
     // if coded correctly, the test yield should equal the reference yield 
