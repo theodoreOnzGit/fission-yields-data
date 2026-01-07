7c864077cee483635d9d43a3090c3220b98c3abb
teddy0_work <theodore_ong@berkeley.edu>
2026-01-07 14:26:34 +0800

    added serde serialise and deserialise

---
+++ examples/serde_experiment/main.rs
@@ -0,0 +1,11 @@
+
+pub fn main() {
+
+    println!("the serde example is here");
+}
+use serde::{Serialize,Deserialize};
+
+#[derive(Debug,Serialize,Deserialize)]
+pub struct NuclideDecayData {
+
+}
